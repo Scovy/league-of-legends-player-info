40a18ffd579a3ae0801ec50934e4ec3bd06bb52e
scov <scovilsyn@gmail.com>
2022-07-09 15:16:48 +0200

    Adding Match history
    Adding match history for last 10 games

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 import SummonerCard from './components/SummonerCard';
+import MatchHistory from './components/MatchHistory';
 function App() {
-  return (
-    <div className="App">
-      <SummonerCard></SummonerCard>
-    </div>
+  const API_KEY = "RGAPI-27f017e4-f515-4019-83b9-569489c735ec"
+  const API_ROUTE = "https://eun1.api.riotgames.com"
+
+  const [playerName, setPlayerName] = useState("")
+  const [summonerData, setSummonerData] = useState("")
+  const [currentPUUID, setCurrentPUUID] = useState("")
+
+
+  function playerSearch(event){
+      let API_CALL = (API_ROUTE + "/lol/summoner/v4/summoners/by-name/" + playerName + "?api_key=" + API_KEY)
+      console.log(API_CALL)
+      axios.get(API_CALL)
+      .then(function(response){
+          setSummonerData(response.data)
+          setCurrentPUUID(response.data.puuid)
+      })
+      .catch(error => console.log("error"))
+  }
+  console.log(summonerData)
+  return(
+      <div className="container">
+      
+      <input 
+      type="text" 
+      value={playerName}
+      onChange={e => setPlayerName(e.target.value)}>
+      </input>
+      <button className="search-player-button" onClick={playerSearch}></button>
+
+      <SummonerCard
+      summonerName={summonerData.name}
+      summonerLevel={summonerData.summonerLevel}
+      puuid={currentPUUID}
+      profileIconId={summonerData.profileIconId}
+      
+      />
+
+      
+
+      
+      
+      
+      <MatchHistory></MatchHistory>
+      </div>
   );
 }
 
---
+++ src/components/MatchHistory.js
@@ -0,0 +1,16 @@
+
+
+
+export default function MatchHistory(){
+
+
+
+    return(
+        <div>
+
+
+
+        </div>
+    )
+
+}
