e7638f9854aedee27dbb737da2e660680e1369c3
Scovy <scovilsyn@gmail.com>
2022-12-05 23:29:02 +0100

    Added styling

--- src/components/SummonerCard.js
+++ src/components/SummonerCard.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import rankImages from './rankImages.json'
 import {Card, CardActions, CardContent, CardMedia, Button, Typography, Grid, TextField, Container, Box} from '@mui/material'
-import { makeStyles } from '@mui/styles';
+import { makeStyles, createTheme } from '@mui/styles';
 import { borderColor } from '@mui/system';
 import styles from './/styles/SummonerCard.css';
 export default function SummonerCard(props){
@@ -13,7 +13,7 @@ const profileIcon = "https://ddragon.leagueoflegends.com/cdn/12.12.1/img/profile
 let tier = props.tier
 let rankIcon = rankImages[tier]
 
-console.log(rankIcon)
+console.log(props.summonerInfo.name)
 
 
 
@@ -21,16 +21,25 @@ console.log(rankIcon)
     return(
       <Container className="container" fixed maxWidth='md' minWidth='xs'>
 
-        <Grid container spacing={2} paddingTop={15}>
-          <Grid className="playerName" container xs={12} alignContent="center" >
-            <Box sx={{display: 'flex', flexDirection: 'row'}}>
+        <Grid container spacing={3}  paddingTop={10}>
+          <Grid className="profileInfo__wrapper" container xs={12} alignContent="center" >
+            <Box className="profileInfo">
               <img className="profileIcon" src={profileIcon} alt='Profile Icon'></img>
+              <Typography  className="profileLevel" lineHeight={1.2} fontSize={12}>{props.summonerInfo.summonerLevel}</Typography>
             </Box>
-              
-
+            <Typography className="profileName" fontWeight={'bold'}>{props.summonerInfo.name}</Typography>
           </Grid>
 
-
+          <Grid container spacing={3}>
+            <Grid item xs={4}>
+              <div className='rankedCard'>This is the first container</div>
+              <div className='rankedCard'>This is the first container</div>
+          </Grid>
+            <Grid item xs={8}>
+              <div>This is the second container</div>
+            </Grid>
+          </Grid>
+          
 
         </Grid>
         
