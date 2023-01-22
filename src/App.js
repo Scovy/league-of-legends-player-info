a3407353a06534b8f2584c667314ea9e20318fdb
Scovy <scovilsyn@gmail.com>
2023-01-22 04:08:44 +0100

    Working backend with searching ability

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import SummonerCard from "./components/SummonerCard";
 import { Button, TextField} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
 import MatchHistory from "./components/MatchHistory";
 import {
   Route,
@@ -11,39 +10,18 @@ import {
   Link,
 } from "react-router-dom";
 function App() {
-  const API_KEY = "RGAPI-289bf4f0-ed7c-414b-8acc-58716a3e69f9"
-  const API_ROUTE = "https://eun1.api.riotgames.com"
 
   const [playerName, setPlayerName] = useState("")
-  const [summonerData, setSummonerData] = useState("")
-  const [summonerQueue, setSummonerQueue] = useState([{}])
-
-
-
-
-function playerSearch(){
-  let API_CALL = (API_ROUTE + "/lol/summoner/v4/summoners/by-name/" + playerName + "?api_key=" + API_KEY)
-  axios.get(API_CALL)
-  .then(function(response){
-    setSummonerData(response.data)  
-  })
-}
-  console.log(playerName)
-
-
-    useEffect(()=>{
-      axios.get(API_ROUTE + "/lol/league/v4/entries/by-summoner/" + summonerData.id + "?api_key=" + API_KEY)
-      .then(function(response){
-        setSummonerQueue(response.data)
+  const [matchList, setMatchList] = useState([])  
+  function playerSearch(){
+    axios.get('http://localhost:4000/games', {params:{nickname:playerName}})
+      .then((response)=> {
+        setMatchList(response.data)
+      }).catch((err)=>{
+        console.log(err)
       })
-    },[summonerData])
+  }
 
-
-  
-
-
-  console.log(summonerData)
-  console.log(summonerQueue)
   return (
 <Routes>        
 
@@ -68,7 +46,7 @@ function playerSearch(){
 }/>
    
     
-      <Route path='/info' element={<SummonerCard {...summonerQueue[0]} summonerInfo={summonerData} playerName />}/>
+      <Route path='/info' element={<SummonerCard  />}/>
     
 
       
