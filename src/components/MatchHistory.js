65574b754859c5aa058cdf4dd0209b233209dd07
Scovy <scovilsyn@gmail.com>
2023-01-25 23:46:41 +0100

    Added ranked queue info: Tier, Lp, Wins, Losses

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -6,28 +6,25 @@ function MatchHistory({ matchData }) {
   return (
     <Grid container spacing={3}>
       {matchDataArray.map((match, index)=>
-        <Grid item xs={12}>
-          <Paper>
-            <Typography variant="h5">Game {index + 1}</Typography>
-            <Grid container >
-              <Grid item xs={6}>
-                {match.info.participants.slice(0, 5).map((data, participantIndex)=>
-                  <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
-                  
-                  {data.summonerName} - {data.championName} </Typography>
-                )}
-              </Grid>
-              <Grid item xs={6}>
-                {match.info.participants.slice(5).map((data, participantIndex)=>
-                  <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
-                  
-                  
-                  {data.summonerName} - {data.championName} </Typography>
-                )}
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
+       <Grid item xs={12}>
+       <Paper style={{width: '50%'}}>  {/*add width here*/}
+         <Typography variant="h5">Game {index + 1}</Typography>
+         <Grid container >
+           <Grid item xs={6}>
+             {match.info.participants.slice(0, 5).map((data, participantIndex)=>
+               <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
+               {data.summonerName} - {data.championName} </Typography>
+             )}
+           </Grid>
+           <Grid item xs={6}>
+             {match.info.participants.slice(5).map((data, participantIndex)=>
+               <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
+               {data.summonerName} - {data.championName} </Typography>
+             )}
+           </Grid>
+         </Grid>
+       </Paper>
+     </Grid>
       )}
     </Grid>
   );
