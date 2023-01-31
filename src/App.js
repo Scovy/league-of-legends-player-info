7499c31a263d86d7a77ba4e09c5be0d7ea259e2e
Scovy <scovilsyn@gmail.com>
2023-01-31 23:18:05 +0100

    Change from MUI to Tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Button, TextField } from "@mui/material";
 import SummonerData from "./components/SummonerData";
 import MatchHistory from "./components/MatchHistory";
 import RankedSideBar from "./components/RankedSideBar";
-import { Route, Routes, Link } from "react-router-dom";
-
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
   const [playerName, setPlayerName] = useState("");
   const [matchList, setMatchList] = useState([{}]);
   const [summonerData, setSummonerData] = useState([]);
-
-  localStorage.setItem("playerName", playerName);
-  localStorage.setItem("matchList", JSON.stringify(matchList));
-  localStorage.setItem("summonerData", JSON.stringify(summonerData));
 
   useEffect(() => {
     playerSearch();
@@ -29,68 +23,102 @@ function App() {
       );
       setSummonerData(summonerResponse.data);
 
-      const matchResponse = await axios.get(
-        "http://localhost:4000/games",
-        { params: { nickname: playerName } }
-      );
+      const matchResponse = await axios.get("http://localhost:4000/games", {
+        params: { nickname: playerName },
+      });
       setMatchList(matchResponse.data);
     } catch (error) {
       console.log(error);
     }
   };
-  console.log(summonerData)
-console.log(summonerData.length)
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/info");
+  };
+
   return (
     <Routes>
       <Route
         path="/"
         element={
-          <div className="container">
-            <img className="emoteImg" src={require("./emote.png")}></img>
+          <div className="container bg-indigo-900 mx-auto">
+            <img className="emoteImg" src={require("./emote.png")} alt=""></img>
             <div className="searchBar">
-              <TextField
-                variant="standard"
-                className="searchbox"
-                type="text"
-                placeholder="Enter your Summoner Name"
-                value={playerName}
-                onChange={(e) => setPlayerName(e.target.value)}
-              ></TextField>
-              <Button
-                component={Link}
-                to="/info"
-                variant="contained"
-                position="center"
-                className="search-player-button"
-              >
-                Search
-              </Button>
+              <form onSubmit={handleSubmit} className="flex items-center">
+                <div className="relative w-full">
+                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    type="text"
+                    required
+                    placeholder="Enter Summoner Name"
+                    value={playerName}
+                    onChange={(e) => setPlayerName(e.target.value)}
+                  ></input>
+                </div>
+
+                <button
+                  type="submit"
+                  class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  <svg
+                    class="w-5 h-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Search</span>
+                </button>
+              </form>
             </div>
           </div>
         }
       />
-      
-      
-        
-       
+
       <Route
-            path="/info"
-            element={
-            <>
+        path="/info"
+        element={
+          <>
             <div className="app_wrapper">
-              <div className="content">
-            {Object.keys(summonerData).length > 0 && <SummonerData summonerInfo={summonerData.summoner}/> }
-            {Object.keys(summonerData).length > 0 && <RankedSideBar queueInfo={summonerData.queue}/>}
-            {matchList.length > 0 && <MatchHistory matchData={matchList} /> }
+              <div className="container">
+                {Object.keys(summonerData).length > 0 && (
+                  <SummonerData summonerInfo={summonerData.summoner} />
+                )}
+                {Object.keys(summonerData).length > 0 && (
+                  <RankedSideBar queueInfo={summonerData.queue} />
+                )}
+                {matchList.length > 0 && <MatchHistory matchData={matchList} playerName={playerName} />}
               </div>
             </div>
-            </>
-            }
-          />
-
-        
+          </>
+        }
+      />
     </Routes>
   );
 }
 
-export default App
+export default App;
--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -1,35 +1,56 @@
-import './styles/MatchHistory.css'
-import { Grid, Paper, Typography } from '@mui/material'
-function MatchHistory({ matchData }) {
-
-
-  const matchDataArray = matchData
-  console.log(matchData)
-  return (
-    <Grid container className="matchhistory_wrapper">
-      {matchDataArray.map((match, index)=>
-       <Grid item>
-       <Paper style={{width: '50%'}}> 
-         <Typography variant="h5">Game {index + 1}</Typography>
-         <Grid container >
-           <Grid item xs={6}>
-             {match.info.participants.slice(0, 5).map((data, participantIndex)=>
+import "./styles/MatchHistory.css";
+/* {match.info.participants.slice(0, 5).map((data, participantIndex)=>
                <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
                {data.summonerName} - {data.championName} </Typography>
              )}
-           </Grid>
-           <Grid item xs={6}>
-             {match.info.participants.slice(5).map((data, participantIndex)=>
+*/
+/* {match.info.participants.slice(5).map((data, participantIndex)=>
                <Typography><img alt='Champion Avatar' className='championAvatar' src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + data.championName + '.png'}/> 
                {data.summonerName} - {data.championName} </Typography>
-             )}
-           </Grid>
-         </Grid>
-       </Paper>
-     </Grid>
-      )}
-    </Grid>
-  );
+*/
+
+function MatchHistory({ matchData, playerName }) {
+  const matchDataArray = matchData;
+
+ 
+
+  return(
+    <div>
+        {matchDataArray.map((match)=> {
+    const participantIndex = match.info.participants.findIndex(participant => participant.summonerName === playerName)
+
+    return (
+    <div className="mt-2 p-2 place-items-center w-96 h-28 bg-indigo-700 flex rounded-xl" >
+          <div className="">
+            <p className="">{match.info.gameMode}</p>
+            <p className="text-justify">{match.info.participants[participantIndex].win ? <p>Win</p> : <p>Lose</p>}</p>
+          </div>
+          <div className="">
+            <img className="h-14 w-14" src={'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion/' + match.info.participants[participantIndex].championName + '.png' } alt="Img"/>
+            {/*Placeholders for summoner spells and runes */}
+          </div>
+          <div>
+            <div className="h-7 w-7 bg-red-600"></div>
+            <div className="h-7 w-7 bg-yellow-600"></div>
+          </div>
+          <div>
+            <div className="h-7 w-7 bg-blue-600"></div>
+          < div className="h-7 w-7 bg-green-600"></div>
+          </div>
+          <div className="pl-4 text-center">
+          <h1 className=" ">{match.info.participants[participantIndex].kills} / {match.info.participants[participantIndex].assists} / {match.info.participants[participantIndex].deaths}</h1>
+          <h2 className="">{Math.round(match.info.participants[participantIndex].challenges.kda * 100) /100 }</h2>
+          <h3>CS {match.info.participants[participantIndex].totalMinionsKilled}</h3>
+          <h4>Vision {match.info.participants[participantIndex].visionScore}</h4>
+          </div>
+    </div>
+    )
+          
+  })}
+
+
+    </div>
+  )
 }
 
 export default MatchHistory;
